44af19306b91401ecba568339a26c968c0470d31
karthikeya0103 <karthikeya_manam@srmap.edu.in>
2025-03-29 16:01:27 +0530

    empty

--- backend/config/db.js
+++
@@ -1,4 +0,0 @@
-import mongoose from "mongoose";
-export const connectDB = async()=>{
-    await mongoose.connect(`mongodb+srv://sivamanik:A12345678b@cluster0.lpzzgep.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`).then(()=>console.log("DB Connected"))
-}
